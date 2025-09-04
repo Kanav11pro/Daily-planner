6f324f6a020d7c4a92af44c084c1ef832ce36b73
Kanav11pro <cbforin@gmail.com>
2025-09-05 00:24:04 +0530

    Update CelebrationController.tsx

--- src/components/celebrations/CelebrationController.tsx
+++ src/components/celebrations/CelebrationController.tsx
@@ -7,7 +7,6 @@ import { ParticleBurstCelebration } from './ParticleBurstCelebration';
 import { GlowingRingCelebration } from './GlowingRingCelebration';
 import { TypewriterCelebration } from './TypewriterCelebration';
 import { NeonGlowCelebration } from './NeonGlowCelebration';
-import { CosmicExplosionCelebration } from './CosmicExplosionCelebration';
 import { BookFlipCelebration } from './BookFlipCelebration';
 import { QuantumFieldCelebration } from './QuantumFieldCelebration';
 import { HologramCelebration } from './HologramCelebration';
